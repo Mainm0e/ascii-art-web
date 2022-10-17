7c7aabe3e8a17713456f5fbad4c2a96afb4a22b9
Mainm0e <mainm0e1996@gmail.com>
2022-10-17 19:07:51 +0300

    hi

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func main(){
+func main() {
 	http.HandleFunc("/", handlerFunc)
 	fmt.Println("Starting the server on :8080...")
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to Ascii-Art-Web<h1>")
 }
