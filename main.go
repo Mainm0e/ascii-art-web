3c43caf8207a77d6b56161b49ca67080bafd0e8d
Mainm0e <mainm0e1996@gmail.com>
2022-10-21 16:22:23 +0300

    hi

--- main.go
+++ main.go
@@ -5,15 +5,23 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+
+	"github.com/mainm0e/asciiweb/rary"
 )
 
 var temp *template.Template
 
+type Data struct {
+	Output    string
+	ErrorNum  int
+	ErrorText string
+}
+
 func main() {
 
-	fileServer := http.FileServer(http.Dir("./ingredients/static"))
+	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/inputAscii", AsciiArtHandle)
+	http.HandleFunc("/ascii-art", AsciiArtHandle)
 	fmt.Println("Start sever at port 8080...")
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -22,13 +30,17 @@ func main() {
 }
 
 func AsciiArtHandle(w http.ResponseWriter, r *http.Request) {
-	type Data struct {
-		Output    string
-		ErrorNum  int
-		ErrorText string
+	fmt.Println(r)
+	if r.URL.Path != "/ascii-art" {
+		fmt.Println("hi")
+		errorHandler(w, r, http.StatusNotFound)
+		return
 	}
+	//err 404 handle
+
 	d := Data{}
-	temp = template.Must(template.ParseGlob("ingredients/static/*.html"))
+	temp = template.Must(template.ParseGlob("static/*.html"))
+
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
@@ -37,10 +49,16 @@ func AsciiArtHandle(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		temp.ExecuteTemplate(w, "index.html", d)
 	} else if r.Method == "POST" {
-		Ascii := r.FormValue("inputText")
-
-		d.Output = Ascii
+		Ascii := r.FormValue("ascii-art")
+		Font := r.FormValue("Font")
+		d.Output = rary.Output(Ascii, Font)
 	}
-	temp.ExecuteTemplate(w, "index.html", d)
+	temp.ExecuteTemplate(w, "resulindex.html", d)
+}
 
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
 }
